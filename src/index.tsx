7df57118769a996642ed418897e1198d27fa3fb0
farukg <fguerlek@gmail.com>
2019-03-20 20:34:31 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { debounce } from 'lodash';
-const ResizeObserver = require('resize-observer-polyfill');
+const ResizeObserver = require('resize-observer-polyfill').default;
 
 interface Props {
   className: string
